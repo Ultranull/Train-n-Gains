df2c835b0f06c8d96f7dd88a2414fa9c68cae00e
rhelkows <rhelk001@odu.edu>
2019-04-18 16:22:02 -0400

    Addition of Congratulations screen

---
+++ app/src/main/java/banana/pudding/pie/train_n_gains/Congradulations.java
@@ -0,0 +1,13 @@
+package banana.pudding.pie.train_n_gains;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Congradulations extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_congradulations);
+    }
+}
